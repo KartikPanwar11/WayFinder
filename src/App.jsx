575a5a57329512dc81221db42b27842eff05467c
KartikPanwar11 <Kartikpanwar1101@gmail.com>
2025-08-28 16:36:57 +0530

    Components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Nav from './Components/Nav'
+import Home from './Components/Home'
+import About from './Components/About'
+import Services from './Components/Services'
+import Tour from './Components/Tour'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Nav/>
+      <Home/>
+      <About/>
+      <Services/>
+      <Tour/>
+      <Footer/>
+    </>
   )
 }
 
